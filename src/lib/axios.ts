94aa32bee180f4b451ba93ef0e247ac673667567
Rahul <chattychidiya@gmail.com>
2024-10-17 04:30:53 +0530

    Update axios baseURL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 
 // Create an instance of Axios with the base URL pointing to port 8000
 const api: AxiosInstance = axios.create({
-    baseURL: "https://api.jobfynder.com/api/v1",
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     withCredentials: true, // Include if you're using cookies for authentication
 });
 
