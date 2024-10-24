3d5ae7838cfe16afb4ed26ed3520c977390d8002
Rahul <chattychidiya@gmail.com>
2024-10-25 01:15:03 +0530

    Add prisma as db

---
+++ src/lib/prisma.ts
@@ -0,0 +1,17 @@
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+    // In production, always use a new PrismaClient instance.
+    prisma = new PrismaClient();
+} else {
+    // In development, use a global variable to preserve the instance across hot reloads.
+    if (!(global as any).prisma) {
+        (global as any).prisma = new PrismaClient();
+    }
+    prisma = (global as any).prisma;
+}
+
+export default prisma;
