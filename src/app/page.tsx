0e0589c829748c2f7e6991a0fda8be360c70204b
Rahul <chattychidiya@gmail.com>
2024-11-01 18:58:05 +0530

    Change route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,26 @@
 import SignOutButton from "@/components/header/logoutButton";
 import { Button } from "@/components/ui/button";
-import { auth, signIn, signOut } from "@/lib/auth";
+import { auth } from "@/lib/auth";
 import Link from "next/link";
 
 export default async function Home() {
-    const session = await auth();
+  const session = await auth();
 
-    if (!session?.user || !session) {
-        return (
-            <div className="grid min-h-screen grid-cols-1 items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-                <h1 className="text-2xl font-bold">You are not logged in</h1>
-                <p className="text-lg">Please login to view this page</p>
-                <Button>
-                    <Link href={"/login"}>Login</Link>
-                </Button>
-            </div>
-        );
-    }
-
+  if (!session || !session?.user) {
     return (
-        <div className="grid min-h-screen grid-cols-1 items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-            Welcome {session.user.name} and your email is {session.user.email}
-            <SignOutButton />
-        </div>
+      <div className="grid min-h-screen grid-cols-1 items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+        <h1 className="text-2xl font-bold">You are not logged in</h1>
+        <p className="text-lg">Please login to view this page</p>
+        <Button>
+          <Link href={"/login"}>Login</Link>
+        </Button>
+      </div>
+    );
+  } else
+    return (
+      <div className="grid min-h-screen grid-cols-1 items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+        Welcome {session.user.name} and your email is {session.user.email}
+        <SignOutButton />
+      </div>
     );
 }
