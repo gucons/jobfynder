8dc27a365a08bce295c8c0b2eba483f0cbc932c6
Rahul <chattychidiya@gmail.com>
2024-11-01 22:11:43 +0530

    Fix redirect

--- src/app/onboarding/role/page.tsx
+++ src/app/onboarding/role/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import ButtonLoading from "@/components/form/buttonLoading";
 import { Button } from "@/components/ui/button";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
@@ -19,146 +20,127 @@ import React, { useState } from "react";
 import { toast } from "sonner";
 
 const roles: Array<{
-    id: UserRole;
-    title: string;
-    description: string;
-    icon: React.ReactNode;
+  id: UserRole;
+  title: string;
+  description: string;
+  icon: React.ReactNode;
 }> = [
-    {
-        id: "CONSULTANT",
-        title: "Consultant",
-        description: "Showcase your skills and find job matches that fit you",
-        icon: <Briefcase className="h-6 w-6" />,
-    },
-    {
-        id: "BENCH_SALES",
-        title: "Bench Sales",
-        description:
-            "Manage consultants on the bench, maintain hotlists and sell bench resources to clients",
-        icon: <TrendingUp className="h-6 w-6" />,
-    },
-    {
-        id: "RECRUITER",
-        title: "Recruiter",
-        description:
-            "Post jobs, connect with consultants, and find talent that matches your hiring needs",
-        icon: <Users className="h-6 w-6" />,
-    },
-    // {
-    //     id: "trainer",
-    //     title: "Trainer",
-    //     description:
-    //         "Create and deliver training programs for IT professionals",
-    //     icon: <GraduationCap className="h-6 w-6" />,
-    // },
+  {
+    id: "CONSULTANT",
+    title: "Consultant",
+    description: "Showcase your skills and find job matches that fit you",
+    icon: <Briefcase className="h-6 w-6" />,
+  },
+  {
+    id: "BENCH_SALES",
+    title: "Bench Sales",
+    description:
+      "Manage consultants on the bench, maintain hotlists and sell bench resources to clients",
+    icon: <TrendingUp className="h-6 w-6" />,
+  },
+  {
+    id: "RECRUITER",
+    title: "Recruiter",
+    description:
+      "Post jobs, connect with consultants, and find talent that matches your hiring needs",
+    icon: <Users className="h-6 w-6" />,
+  },
+  // {
+  //     id: "trainer",
+  //     title: "Trainer",
+  //     description:
+  //         "Create and deliver training programs for IT professionals",
+  //     icon: <GraduationCap className="h-6 w-6" />,
+  // },
 ];
 
 export default function RoleSelectionPage() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const [selectedRole, setSelectedRole] = useState("");
-    const [loading, setLoading] = useState(false);
+  const [selectedRole, setSelectedRole] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const handleRoleChange = (value: string) => {
-        setSelectedRole(value);
-    };
+  const handleRoleChange = (value: string) => {
+    setSelectedRole(value);
+  };
 
-    const handleContinue = async () => {
-        setLoading(true);
-        try {
-            const response = await axios.post("/api/user/profile/role", {
-                role: selectedRole,
-            });
-            if (response.status === 200) {
-                toast.success("Role updated successfully", {});
+  const handleContinue = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.post("/api/user/profile/role", {
+        role: selectedRole,
+      });
+      if (response.status === 200) {
+        toast.success("Role updated successfully", {});
 
-                router.push("/get-started/basic-info");
-            }
-        } catch (error) {
-            showToastError(error);
-        } finally {
-            setLoading(false);
-        }
-    };
+        router.push("/onboarding/basic-info");
+      }
+    } catch (error) {
+      showToastError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <div className="flex min-h-screen flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <h1 className="text-center text-3xl font-extrabold text-gray-900">
-                    Welcome to Jobfynder!
-                </h1>
-                <p className="mt-2 space-y-4 text-center text-sm text-gray-600">
-                    We&apos;re here to make sure you have the best experience.
-                    {/* <br /> */}
-                    {/* Let&apos;s get started by understanding your role so we can
+  return (
+    <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h1 className="text-center text-3xl font-extrabold text-gray-900">
+          Welcome to Jobfynder!
+        </h1>
+        <p className="mt-2 space-y-4 text-center text-sm text-gray-600">
+          We&apos;re here to make sure you have the best experience.
+          {/* <br /> */}
+          {/* Let&apos;s get started by understanding your role so we can
                     customize your profile. */}
-                </p>
-            </div>
+        </p>
+      </div>
 
-            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg">
-                <Card>
-                    <CardHeader>
-                        <CardTitle>Select Your Role</CardTitle>
-                        <CardDescription>
-                            Choose the option that best describes your primary
-                            role
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <RadioGroup
-                            onValueChange={handleRoleChange}
-                            className="space-y-4"
-                        >
-                            {roles.map((role) => (
-                                <div
-                                    key={role.id}
-                                    className="flex items-center space-x-2"
-                                >
-                                    <RadioGroupItem
-                                        value={role.id}
-                                        id={role.id}
-                                        className="peer sr-only"
-                                    />
-                                    <Label
-                                        htmlFor={role.id}
-                                        className={`flex w-full cursor-pointer items-center space-x-4 rounded-lg border border-gray-200 p-4 hover:bg-gray-100 peer-checked:border-purple-600 peer-checked:bg-purple-50 [&:has([data-state=checked])]:border-purple-600 [&:has([data-state=checked])]:bg-purple-50 ${
-                                            selectedRole === role.id
-                                                ? "border-green-600 bg-green-50 hover:bg-green-50 peer-checked:border-green-600 peer-checked:bg-green-50"
-                                                : ""
-                                        } `}
-                                    >
-                                        <div className="flex-shrink-0">
-                                            {role.icon}
-                                        </div>
-                                        <div className="flex-grow">
-                                            <div className="font-semibold">
-                                                {role.title}
-                                            </div>
-                                            <div className="text-sm text-gray-500">
-                                                {role.description}
-                                            </div>
-                                        </div>
-                                    </Label>
-                                </div>
-                            ))}
-                        </RadioGroup>
-                        <Button
-                            onClick={handleContinue}
-                            className="mt-6 w-full"
-                            disabled={!selectedRole}
-                        >
-                            {loading ? (
-                                <span className="flex items-center justify-center">
-                                    <Loader className="mr-2 size-4 animate-spin" />
-                                    Loading
-                                </span>
-                            ) : (
-                                "Continue"
-                            )}
-                        </Button>
-                    </CardContent>
-                </Card>
-            </div>
-        </div>
-    );
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg">
+        <Card>
+          <CardHeader>
+            <CardTitle>Select Your Role</CardTitle>
+            <CardDescription>
+              Choose the option that best describes your primary role
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <RadioGroup onValueChange={handleRoleChange} className="space-y-2">
+              {roles.map((role) => (
+                <div key={role.id} className="flex items-center">
+                  <RadioGroupItem
+                    value={role.id}
+                    id={role.id}
+                    className="peer sr-only"
+                  />
+                  <Label
+                    htmlFor={role.id}
+                    className={`flex w-full m-0 cursor-pointer items-center space-x-4 rounded-lg border border-gray-200 p-4 hover:bg-gray-100 peer-checked:border-purple-600 peer-checked:bg-purple-50 [&:has([data-state=checked])]:border-purple-600 [&:has([data-state=checked])]:bg-purple-50 ${
+                      selectedRole === role.id
+                        ? "border-green-600 bg-green-50 hover:bg-green-50 peer-checked:border-green-600 peer-checked:bg-green-50"
+                        : ""
+                    } `}
+                  >
+                    <div className="flex-shrink-0">{role.icon}</div>
+                    <div className="flex-grow">
+                      <div className="font-semibold">{role.title}</div>
+                      <div className="text-sm text-gray-500">
+                        {role.description}
+                      </div>
+                    </div>
+                  </Label>
+                </div>
+              ))}
+            </RadioGroup>
+            <ButtonLoading
+              onClick={handleContinue}
+              className="mt-6 w-full"
+              disabled={!selectedRole}
+              loading={loading}
+            />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/form/buttonLoading.tsx
@@ -0,0 +1,34 @@
+import { Button, ButtonProps } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Loader } from "lucide-react";
+import React from "react";
+
+const ButtonLoading = React.forwardRef<
+  HTMLButtonElement,
+  ButtonProps & { loading?: boolean }
+>(
+  (
+    { className, variant, size, asChild = false, loading = false, ...props },
+    ref
+  ) => {
+    return (
+      <Button
+        className={cn("w-full", className)}
+        ref={ref}
+        {...props}
+        disabled={loading}
+      >
+        {loading ? (
+          <span className="flex items-center justify-center">
+            <Loader className="mr-2 size-4 animate-spin" />
+            Loading
+          </span>
+        ) : (
+          "Continue"
+        )}
+      </Button>
+    );
+  }
+);
+
+export default ButtonLoading;
