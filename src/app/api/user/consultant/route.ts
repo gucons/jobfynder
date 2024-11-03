bc7ce0030c0dff55269d060d92f58c30f15704b2
Rahul <chattychidiya@gmail.com>
2024-11-03 20:37:55 +0530

    Create route to handle consultant data

--- src/app/api/user/consultant/route.ts
+++ src/app/api/user/consultant/route.ts
@@ -6,52 +6,58 @@ import { sendErrorResponse, sendSuccessResponse } from "@/server/response";
 import { z } from "zod";
 
 export async function POST(req: Request) {
-    try {
-        const session = await authenticateUser();
+  try {
+    const session = await authenticateUser();
 
-        const requestData = await req.json();
-        const data = ConsultantSchema.parse(requestData);
+    const requestData = await req.json();
+    const data = ConsultantSchema.parse(requestData);
 
-        const consultant = await prisma.consultant.upsert({
-            where: {
-                userId: session.user.id,
-            },
-            update: {
-                ...data,
-                education: {
-                    set: data.education ? data.education.map((edu: any) => ({ ...edu })) : [],
-                },
-            },
-            create: {
-                ...data,
-                education: {
-                    create: data.education ? data.education.map((edu: any) => ({ ...edu })) : [],
-                },
-                userId: session.user.id,
-            },
-        });
+    const consultant = await prisma.consultant.upsert({
+      where: {
+        userId: session.user.id,
+      },
+      update: {
+        ...data,
+        education: {
+          set: data.education
+            ? data.education.map((edu: any) => ({ ...edu }))
+            : [],
+        },
+      },
+      create: {
+        ...data,
+        education: {
+          create: data.education
+            ? data.education.map((edu: any) => ({ ...edu }))
+            : [],
+        },
+        userId: session.user.id,
+      },
+    });
 
-        if (!consultant) {
-            return sendErrorResponse({
-                success: false,
-                message: "Consultant not found",
-            });
-        }
-
-        return sendSuccessResponse({
-            success: true,
-            message: "Consultant details updated successfully",
-        });
-    } catch (error: any) {
-        if (error instanceof z.ZodError) {
-            return handleZodErrorResponse(error);
-        } else {
-            console.log("Error updating user role", error.message || error);
-            return sendErrorResponse({
-                success: false,
-                message: "Error updating user role",
-                error: error.message || error,
-            });
-        }
+    if (!consultant) {
+      return sendErrorResponse({
+        success: false,
+        message: "Consultant not found",
+      });
     }
+
+    return sendSuccessResponse({
+      success: true,
+      message: "Consultant details updated successfully",
+    });
+  } catch (error: any) {
+    if (error instanceof z.ZodError) {
+      console.log("Error updating consultant details:", error.errors);
+
+      return handleZodErrorResponse(error);
+    } else {
+      console.log("Error updating consultant details:", error.message || error);
+      return sendErrorResponse({
+        success: false,
+        message: "Error updating consultant details",
+        error: error.message || error,
+      });
+    }
+  }
 }
