c683acc88dec8fad8932aae040c82a7b079c2ec4
Rahul <chattychidiya@gmail.com>
2024-12-09 21:52:49 +0530

    Fix posts route

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,15 +2,13 @@ import prisma from "@/lib/prisma";
 import handleRoute from "@/server/handleRoutes";
 import { NextResponse } from "next/server";
 
-export default function GET(req: Request) {
-  handleRoute(async (req) => {
-    const posts = await prisma.post.findMany({
-      orderBy: {
-        createdAt: "asc",
-      },
-      take: 10,
-    });
-
-    return NextResponse.json({ posts: posts });
+export const GET = handleRoute(async (req: Request) => {
+  const posts = await prisma.post.findMany({
+    orderBy: {
+      createdAt: "asc",
+    },
+    take: 10,
   });
-}
+
+  return NextResponse.json({ posts: posts });
+});
